3d313dcc410350b777a412382a490d6ba6bec0bb
Cheryl246 <cheryl.valencia@binus.ac.id>
2025-04-20 16:41:39 +0700

    Update UTSprediction1.py

--- UTSprediction1.py
+++ UTSprediction1.py
@@ -11,14 +11,12 @@ room_type=joblib.load('room_type.pkl')
 market_segment=joblib.load('market_segment.pkl')
 
 def main():
-    st.title('Hotel Reservation Prediction')
-
-    # Input data dari pengguna
+    st.title('Hotel Reservastion Prediction')
     Booking_ID = st.text_input("Booking_ID")
     no_of_adults = st.selectbox("Adults", options=range(1,11))
-    no_of_children = st.selectbox("Children, under 17", options=range(0,11))
+    no_of_children = st.selectbox("Children, under 17", options=range(1,11))
     no_of_weekend_nights = st.number_input("Weekend Nights", 0, 8)
-    no_of_week_nights = st.number_input("Week Nights (Mon to Fri)", 0, 17)
+    no_of_week_nights = st.number_input("Week Nights (Mon to Fri)", 0, 7)
     type_of_meal_plan = st.selectbox("Type of meal plan", ["Meal Plan 1", "Meal Plan 2", "Meal Plan 3","Not Selected"])
     required_car_parking_space = st.radio("Required car parking space", [0, 1])
     room_type_reserved = st.selectbox("Room type reserved", ["Room_Type 1", "Room_Type 2", "Room_Type 3", "Room_Type 4","Room_Type 5","Room_Type 6","Room_Type 7"])
@@ -30,56 +28,53 @@ def main():
     repeated_guest = st.radio("Repeated guest", [0, 1])
     no_of_previous_cancellations = st.number_input("Number of previous cancellations", 0, 30)
     no_of_previous_bookings_not_canceled = st.number_input("Number of previous bookings not canceled", 0, 30)
-    avg_price_per_room = st.number_input("Average price per room (in Euros)", min_value=0.0, format="%.2f")
+    avg_price_per_room = st.text_input("Average price per room (in Euros)")
     no_of_special_requests = st.number_input("Number of special requests", 0, 10)
-
-    # Membuat DataFrame dari input pengguna - menggunakan nama kolom yang konsisten
+    
     data = {
-        'no_of_adults': no_of_adults,
-        'no_of_children': no_of_children,
-        'no_of_weekend_nights': no_of_weekend_nights,
-        'no_of_week_nights': no_of_week_nights,
-        'type_of_meal_plan': type_of_meal_plan,
-        'required_car_parking_space': required_car_parking_space,
-        'room_type_reserved': room_type_reserved,
-        'lead_time': lead_time,
-        'arrival_year': arrival_year,
-        'arrival_month': arrival_month,
-        'arrival_date': arrival_date,
-        'market_segment_type': market_segment_type,
-        'repeated_guest': repeated_guest,
-        'no_of_previous_cancellations': no_of_previous_cancellations,
-        'no_of_previous_bookings_not_canceled': no_of_previous_bookings_not_canceled,
-        'avg_price_per_room': avg_price_per_room,
-        'no_of_special_requests': no_of_special_requests
-    }
+        'Booking ID': Booking_ID,
+        'Number of adults': no_of_adults,
+        'Number of children': no_of_children,
+        'Number of weekend nights': no_of_weekend_nights,
+        'Number of week nights': no_of_week_nights,
+        'Type of meal plan': type_of_meal_plan,
+        'Required car parking space': required_car_parking_space,
+        'Room type reserved': room_type_reserved,
+        'Lead time': lead_time,
+        'Arrival year': arrival_year,
+        'Arrival month': arrival_month,
+        'Arrival date': arrival_date,
+        'Market segment type': market_segment_type,
+        'Repeated guest': repeated_guest,
+        'Number of previous cancellations': no_of_previous_cancellations,
+        'Number of previous bookings not canceled': no_of_previous_bookings_not_canceled,
+        'Average price per room': avg_price_per_room,
+        'Number of special requests': no_of_special_requests }
+        
+    data = pd.DataFrame([list(data.values())], columns=['Booking ID', 'Number of adults', 'Number of children', 'Number of weekend nights', 
+                                                  'Number of week nights', 'Type of meal plan', 'Required car parking space', 
+                                                  'Room type reserved', 'Lead time', 'Arrival year', 'Arrival month', 'Arrival date', 
+                                                  'Market segment type', 'Repeated guest', 'Number of previous cancellations', 
+                                                  'Number of previous bookings not canceled', 'Average price per room', 
+                                                  'Number of special requests'])
+    st.write(data.iloc[:, 0])  
 
-    input_df = pd.DataFrame([data])
 
     if st.button('Make Prediction'):
-        try:
-            # Encode categorical variables - using consistent column names
-            input_df['type_of_meal_plan'] = meal_plane.transform(input_df['type_of_meal_plan'].astype(str))
-            input_df['room_type_reserved'] = room_type.transform(input_df['room_type_reserved'].astype(str))
-            input_df['market_segment_type'] = market_segment.transform(input_df['market_segment_type'].astype(str))
-            
-    
-            # Ensure all columns are numeric
-            input_df = input_df.astype(float)
-            
-            # Make prediction
-            prediction = model.predict(input_df)
-            
-            # Decode prediction
-            decoded_prediction = target_encoded.inverse_transform(prediction.reshape(-1, 1))
-            
-            # Show prediction
-            st.success(f'The prediction is: {decoded_prediction[0][0]}')
-            
-            # Show predictio   
-        except Exception as e:
-            st.error(f"An error occurred: {str(e)}")
+        if 'Type of meal plan' in data.columns and 'Room type reserved' in data.columns and 'Market segment type' in data.columns:
+            data['Type of meal plan'] = meal_plane.transform(data['Type of meal plan'])
+            data['Room type reserved'] = room_type.transform(data['Room type reserved'])
+            data['Market segment type'] = market_segment.transform(data['Market segment type'])
+            if 'Booking ID' in data.columns:
+                features = data.drop(['Booking ID'], axis=1)
+        else:
+            features = data
+        features = features.fillna(0).astype(float)
+        prediction = model.predict(features)
+        decoded_prediction = target_encoded.inverse_transform(prediction)
+        st.success(f'The prediction is: {decoded_prediction[0]}')
+    else:
+        st.error("Required columns are missing in the input data.")
 
 if __name__ == '__main__':
     main()
-
